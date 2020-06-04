34ad48a633098ab4ec6caeab5bca11e9844d9f4b
Smiletoforget <1367159064@qq.com>
2020-06-05 00:31:22 +0800

    初始化项目

---
+++ src/main/java/cn/smile/core/MSpringBootConfiguration.java
@@ -0,0 +1,17 @@
+package cn.smile.core;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author smiletofotget
+ * @creationTime 2020-06-2020/6/4
+ */
+@SpringBootApplication
+@SpringBootConfiguration
+public class MSpringBootConfiguration {
+	
+	public static void main(String[] args) {
+	
+	}
+}
