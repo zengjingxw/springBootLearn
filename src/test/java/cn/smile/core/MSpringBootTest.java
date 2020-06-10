2244e67c3fbd52b0c6701e745ec40dfe1530ebf6
Smiletoforget <1367159064@qq.com>
2020-06-10 12:55:17 +0800

    带有测试的原始目录

---
+++ src/test/java/cn/smile/core/MSpringBootTest.java
@@ -0,0 +1,20 @@
+package cn.smile.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author smiletofotget
+ * @creationTime 2020-06-2020/6/10
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MSpringBootTest {
+	
+	@Test
+	public void test() {
+	
+	}
+}
